781587d28d9d9f482b5f4e39c440bc2cb0d08d21
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-04-15 23:57:05 +0800

    add ruby version

---
+++ ruby/test/roulette_wheel_selection_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class RouletteWheelSelectionTest < Minitest::Test
+  def setup
+    
+  end
+
+  def test_hash_parameter
+    
+  end
+end
---
+++ ruby/test/test_helper.rb
@@ -0,0 +1,8 @@
+require "simplecov"
+SimpleCov.start
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'roulette-wheel-selection'
+
+require 'minitest/autorun'
+
