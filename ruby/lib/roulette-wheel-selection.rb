130fd61afcb710a25c65b115ef83889549e3233c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-04-16 00:18:19 +0800

    support array input

--- ruby/lib/roulette-wheel-selection.rb
+++ ruby/lib/roulette-wheel-selection.rb
@@ -2,7 +2,23 @@ require "roulette-wheel-selection/version"
 
 class RouletteWheelSelection
   class << self
-    def sample(hash) # [obj => prop.]
+    def sample(*args)
+      object = args.first
+      case object
+      when Array ; sample_from_array(*args)
+      when Hash  ; sample_from_hash(*args)
+      else       ; fail "Unsupported type: #{object.class}"
+      end
+    end
+
+    private
+
+    def sample_from_array(array, key)
+      hash = array.map{|v| [v, v[key]] }.to_h
+      return sample_from_hash(hash)
+    end
+
+    def sample_from_hash(hash)
       total_rate = hash.values.inject(&:+)
       random_seed = rand(total_rate)
       hash.each do |obj, rate|
--- ruby/test/roulette_wheel_selection_test.rb
+++ ruby/test/roulette_wheel_selection_test.rb
@@ -8,4 +8,19 @@ class RouletteWheelSelectionTest < Minitest::Test
   def test_hash_parameter
     assert_equal 'b', RouletteWheelSelection.sample('a' => 10, 'b' => 20, 'c' => 30)
   end
+
+  def test_array_parameter
+    objects = [
+      {name: 'a', weight: 10},
+      {name: 'b', weight: 20},
+      {name: 'c', weight: 30},
+    ]
+    assert_equal objects[1], RouletteWheelSelection.sample(objects, :weight)
+  end
+
+  def test_string_parameter
+    assert_raises RuntimeError do 
+      RouletteWheelSelection.sample('This is string')
+    end
+  end
 end
