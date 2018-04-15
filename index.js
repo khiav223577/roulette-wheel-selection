b6a20f1b806ad4270c90d147f0a402a4e2ff335f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-04-15 23:24:43 +0800

    implement RouletteWheelSelection

---
+++ index.js
@@ -0,0 +1,13 @@
+function RouletteWheelSelection(objects, fAttr){ // object = { attr1: xx1, attr2, xx2 }
+  var totalFitness = 0;
+  objects.forEach(function(object){ totalFitness += object[fAttr]; });
+  var seed = Math.floor(Math.random() * totalFitness);
+  for(var i = 0; i < objects.length; ++i){
+    var object = objects[i];
+    var rate = object[fAttr];
+    if (seed < rate) return object;
+    seed -= rate;
+  }
+}
+
+module.exports = RouletteWheelSelection;
